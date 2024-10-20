80ad85a7e2990abaa658c77bb7f72f2ec8dd7a71
Philip Johnson <p.zhao.johnson@gmail.com>
2024-10-20 10:30:37 -0400

    Set cursor and background color CSS styles in Drawing class constructor

--- src/Drawing.ts
+++ src/Drawing.ts
@@ -65,9 +65,13 @@ export class Drawing {
     this.domNode.setAttribute('width', `${defaultWidth}`);
     this.domNode.setAttribute('height', `${defaultHeight}`);
 
+    this.domNode.style.backgroundColor = 'white';
+
     // prevent highlighting of text when dragging drawing elements
     this.domNode.style.userSelect = 'none';
     this.domNode.style.webkitUserSelect = 'none';
+
+    this.domNode.style.cursor = 'default';
 
     this.basesDrawing = new BasesDrawing(this.domNode, []);
 
