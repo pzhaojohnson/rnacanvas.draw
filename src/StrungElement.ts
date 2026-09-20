f64cd6a152af112c9fd920f0d6961ee992757f58
Philip Johnson <p.zhao.johnson@gmail.com>
2026-09-19 20:57:14 -0400

    Fix strung element type annotations

--- src/StrungElement.ts
+++ src/StrungElement.ts
@@ -1,3 +1,12 @@
 import type { StrungText, StrungCircle, StrungRectangle, StrungTriangle } from '@rnacanvas/draw.floating';
 
-export type StrungElement = StrungText | StrungCircle | StrungRectangle | StrungTriangle;
+import type { Text, Circle, Rectangle, Triangle } from '@rnacanvas/draw.floating';
+
+import type { Bond } from './Bond';
+
+export type StrungElement = (
+  StrungText<Text, Bond>
+  | StrungCircle<Circle, Bond>
+  | StrungRectangle<Rectangle, Bond>
+  | StrungTriangle<Triangle, Bond>
+);
